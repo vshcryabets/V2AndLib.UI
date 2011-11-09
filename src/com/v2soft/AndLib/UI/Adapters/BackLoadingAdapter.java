c11ba24521d0cb7a8aeeb9f3c4f7dbab4b527ac2
Vladimir Shcriabets <vshcryabets@gmail.com>
2011-11-09 18:16:06 +0200

    License added to BackLoadAdapter

--- src/com/v2soft/AndLib/UI/Adapters/BackLoadingAdapter.java
+++ src/com/v2soft/AndLib/UI/Adapters/BackLoadingAdapter.java
@@ -1,3 +1,23 @@
+// ***** BEGIN LICENSE BLOCK *****
+// Version: MPL 1.1
+// 
+// The contents of this file are subject to the Mozilla Public License Version 
+// 1.1 (the "License"); you may not use this file except in compliance with 
+// the License. You may obtain a copy of the License at 
+// http://www.mozilla.org/MPL/
+// 
+// Software distributed under the License is distributed on an "AS IS" basis,
+// WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+// for the specific language governing rights and limitations under the
+// License.
+// 
+// The Initial Developer of the Original Code is 
+//  2V Software (vshcryabets@2vsoft.com).
+// Portions created by the Initial Developer are Copyright (C) 2010
+// the Initial Developer. All Rights Reserved.
+// 
+// 
+// ***** END LICENSE BLOCK *****
 package com.v2soft.AndLib.UI.Adapters;
 
 import java.util.List;
@@ -8,6 +28,11 @@ import android.os.Handler.Callback;
 import android.os.Message;
 import android.widget.BaseAdapter;
 
+/**
+ * 
+ * @author V.Shcriyabets (vshcryabets@gmail.com)
+ *
+ */
 public abstract class BackLoadingAdapter<T> extends BaseAdapter implements Callback {
     //---------------------------------------------------------------------------
     // Constants
