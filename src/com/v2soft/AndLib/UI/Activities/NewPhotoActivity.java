99d8a62aa52946c3229e1cfb36af1c818096f674
Vladimir Shcriabets <vshcryabets@gmail.com>
2011-11-09 19:10:54 +0200

    Activity for for taking photographs

---
+++ src/com/v2soft/AndLib/UI/Activities/NewPhotoActivity.java
@@ -0,0 +1,136 @@
+// ***** BEGIN LICENSE BLOCK *****
+// Version: MPL 1.1
+// 
+// The contents of this file are subject to the Mozilla Public License Version 
+// 1.1 (the "License"); you may not use this file except in compliance with 
+// the License. You may obtain a copy of the License at 
+// http://www.mozilla.org/MPL/
+// 
+// Software distributed under the License is distributed on an "AS IS" basis,
+// WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+// for the specific language governing rights and limitations under the
+// License.
+// 
+// The Initial Developer of the Original Code is 
+//  2V Software (vshcryabets@2vsoft.com).
+// Portions created by the Initial Developer are Copyright (C) 2010
+// the Initial Developer. All Rights Reserved.
+// 
+// 
+// ***** END LICENSE BLOCK *****
+package com.v2soft.AndLib.UI.Activities;
+
+import com.v2soft.AndLib.UI.CameraView;
+
+import android.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.FrameLayout.LayoutParams;
+
+public class NewPhotoActivity extends Activity implements OnClickListener
+{
+	//----------------------------------------------------------------------------------------------
+	// Constants	
+	//----------------------------------------------------------------------------------------------
+	public static final String ACTIVITY_RESULT_FILENAME = "RESULT";
+	public static final String ACTIVITY_PARAM_FILENAME = "FILENAME";
+    private static final String LOG_TAG = NewPhotoActivity.class.getSimpleName();
+	//----------------------------------------------------------------------------------------------
+	// Variables	
+	//----------------------------------------------------------------------------------------------	
+	private CameraView mCameraView;
+	private ImageButton mTakePhotoUI;
+	private String mPhotoFileName;
+	//----------------------------------------------------------------------------------------------
+	// Activity routines	
+	//----------------------------------------------------------------------------------------------	
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		try
+		{
+			super.onCreate(savedInstanceState);
+			requestWindowFeature(Window.FEATURE_NO_TITLE);
+			getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+					WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+			Bundle extras = this.getIntent().getExtras();
+			mPhotoFileName = extras.getString("FILENAME");
+
+			mCameraView = new CameraView(this.getApplicationContext());
+			FrameLayout rl = new FrameLayout(this.getApplicationContext());
+			setContentView(rl);
+			rl.addView(mCameraView);           
+			mCameraView.setOnClickListener(this);
+			mTakePhotoUI = new ImageButton(this);
+			mTakePhotoUI.setImageResource(R.drawable.ic_menu_camera);
+			mTakePhotoUI.setOnClickListener(this);
+			LayoutParams params = new LayoutParams(LayoutParams.WRAP_CONTENT,
+					LayoutParams.WRAP_CONTENT);
+			params.gravity = Gravity.RIGHT;
+			rl.addView(mTakePhotoUI, params);
+		} 
+		catch(Exception e) {
+		    Log.e(LOG_TAG, e.toString(), e);
+		}
+	}
+
+	@Override
+	protected void onDestroy() 
+	{
+		super.onDestroy();
+	}
+
+	@Override
+	public boolean onKeyUp(int keyCode, KeyEvent event) 
+	{
+		if ( ( keyCode == KeyEvent.KEYCODE_DPAD_CENTER ) ||
+				( keyCode == KeyEvent.KEYCODE_CAMERA ) )
+		{
+			takePhoto();
+			return true;
+		}
+		return super.onKeyUp(keyCode, event);
+	}
+
+	public void onClick(View v) 
+	{
+		if ( v.equals(mCameraView) )
+		{
+			// return result and close activity
+			Intent resultIntent = new Intent();
+			resultIntent.putExtra(ACTIVITY_RESULT_FILENAME, mCameraView.getFileName());
+			setResult(Activity.RESULT_OK, resultIntent);
+			finish();
+		}
+		if ( v.equals(mTakePhotoUI))
+		{
+			takePhoto();
+		}
+	}
+
+	private void takePhoto() 
+	{
+//		String path = Config.getInstance().getCacheDir()+
+//		File.separator+UUID.randomUUID()+".jpg";
+//		boolean portrait = false;
+//		if ( ( current_orientation < 360 ) && ( current_orientation > 310 ))
+//			portrait = true;
+//		if ( ( current_orientation < 230 ) && ( current_orientation > 130 ))
+//			portrait = true;
+//		if ( ( current_orientation < 50 ) && ( current_orientation > -1 ))
+//			portrait = true;
+		mCameraView.takePicture(mPhotoFileName, false, false);	
+	}
+}
---
+++ src/com/v2soft/AndLib/UI/CameraView.java
@@ -0,0 +1,169 @@
+// ***** BEGIN LICENSE BLOCK *****
+// Version: MPL 1.1
+// 
+// The contents of this file are subject to the Mozilla Public License Version 
+// 1.1 (the "License"); you may not use this file except in compliance with 
+// the License. You may obtain a copy of the License at 
+// http://www.mozilla.org/MPL/
+// 
+// Software distributed under the License is distributed on an "AS IS" basis,
+// WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+// for the specific language governing rights and limitations under the
+// License.
+// 
+// The Initial Developer of the Original Code is 
+//	2V Software (vshcryabets@2vsoft.com).
+// Portions created by the Initial Developer are Copyright (C) 2010
+// the Initial Developer. All Rights Reserved.
+// 
+// 
+// ***** END LICENSE BLOCK *****
+package com.v2soft.AndLib.UI;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.hardware.Camera.PictureCallback;
+import android.provider.MediaStore;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class CameraView extends SurfaceView implements PictureCallback
+{
+	private static final String LOG_TAG = CameraView.class.getSimpleName();
+    private String out_file_name = null;
+	private Camera mCamera;
+	private SurfaceHolder mPreviewHolder;
+	private boolean preview_running = false;
+	private OnClickListener listener = null;
+
+    public CameraView(Context context) {
+        this(context,null);
+    }
+    public CameraView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+	public CameraView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+
+		mPreviewHolder = this.getHolder();
+		mPreviewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		mPreviewHolder.addCallback(surfaceHolderListener);
+	}
+
+	public void setOnClickListener(OnClickListener listener)
+	{
+		this.listener = listener;
+	}
+	
+	private void updatePreviewState(int width, int height)
+	{
+		try
+		{
+			if ( preview_running )
+			{
+				mCamera.stopPreview();
+			}
+			Parameters params = mCamera.getParameters();
+			params.setPreviewSize(width, height);
+			params.setPictureFormat(PixelFormat.JPEG);
+			mCamera.setParameters(params);
+			mCamera.setPreviewDisplay(mPreviewHolder);
+			mCamera.startPreview();
+			preview_running = true;
+		}
+		catch (Exception e) 
+		{
+			Log.e(LOG_TAG, e.toString(), e);
+		}
+	}
+
+	SurfaceHolder.Callback surfaceHolderListener = new SurfaceHolder.Callback() 
+	{
+		public void surfaceCreated(SurfaceHolder holder) 
+		{
+			try {
+			    Log.d(LOG_TAG, "open camera");
+				mCamera = Camera.open();
+				mCamera.setPreviewDisplay(mPreviewHolder);
+			}
+            catch (Exception e) {
+                Log.e(LOG_TAG, e.toString(), e);
+            }           
+		}
+
+		public void surfaceChanged(SurfaceHolder holder, int format, int width,
+				int height)	{
+			try	{
+				updatePreviewState(width, height);
+			}
+			catch (Exception e) {
+			    Log.e(LOG_TAG, e.toString(), e);
+			}			
+		}
+
+		public void surfaceDestroyed(SurfaceHolder arg0) {
+			mCamera.stopPreview();
+			preview_running = false;
+			mCamera.release();
+			mCamera = null;
+		}
+	};
+
+	public void takePicture(String path, boolean portrait, boolean flash_disable ) 
+	{
+		if ( mCamera == null ) return;
+		out_file_name = path;
+		Parameters parameters =  mCamera.getParameters();
+		parameters.setPictureFormat(PixelFormat.JPEG);
+		if ( portrait )
+		{
+			parameters.set("rotation", 90);
+		}
+		if ( flash_disable )
+			parameters.set("flash-mode", "off");
+		mCamera.setParameters(parameters);
+		mCamera.takePicture(null,null,this);
+	}
+
+	public void onPictureTaken(byte[] data, Camera camera) 
+	{
+		if ( data == null) return;
+		if ( out_file_name == null ) return;
+		FileOutputStream out = null;
+		try 
+		{
+			out = new FileOutputStream(out_file_name);
+			out.write(data);
+			// register in MediaStore
+			MediaStore.Images.Media.insertImage(
+					getContext().getContentResolver(), 
+					out_file_name, null, null);
+			
+			if ( listener!=null )
+				listener.onClick(this);
+		} catch (Exception e) 
+		{
+		    Log.e(LOG_TAG, e.toString(), e);
+		}
+		finally
+		{
+			if ( out != null )
+				try {out.close();}catch (IOException e){}
+			if ( preview_running )
+				mCamera.stopPreview();
+			mCamera.startPreview();
+		}
+	}
+
+	public String getFileName() 
+	{
+		return out_file_name;
+	}
+}
