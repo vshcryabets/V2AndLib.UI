1e75a557203e326e3114c675faa4292932588025
Vladimir Shcriabets <vshcryabets@gmail.com>
2012-07-03 16:40:08 +0300

    - bonded bluetooth device will be shown at list

--- V2Demo/src/com/v2soft/V2AndLib/demoapp/ui/fragments/DemoBluetoothDeviceList.java
+++ V2Demo/src/com/v2soft/V2AndLib/demoapp/ui/fragments/DemoBluetoothDeviceList.java
@@ -1,7 +1,15 @@
 package com.v2soft.V2AndLib.demoapp.ui.fragments;
 
 import android.app.Fragment;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.view.View;
+import android.widget.AdapterView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
 
 import com.v2soft.AndLib.ui.fragments.BluetoothDeviceList;
 import com.v2soft.V2AndLib.demoapp.DemoAppSettings;
@@ -14,13 +22,38 @@ import com.v2soft.V2AndLib.demoapp.DemoApplication;
  */
 public class DemoBluetoothDeviceList 
 extends BluetoothDeviceList<DemoApplication, DemoAppSettings>  {
+    private static final UUID SPP_UUID = 
+            UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    
     public static Fragment newInstance() {
         return new DemoBluetoothDeviceList();
     }
     
     @Override
     public void onClick(View v) {
-        // TODO Auto-generated method stub
-        
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,
+            long id) {
+        BluetoothDevice device = (BluetoothDevice) mAdapter.getItem(position);
+        try {
+            BluetoothSocket socket = device.createRfcommSocketToServiceRecord(SPP_UUID);
+            socket.connect();
+            InputStream ins = socket.getInputStream();
+            OutputStream outs = socket.getOutputStream();
+            outs.write("AT\n".getBytes());
+            outs.flush();
+            byte[] buffer = new byte[1024];
+            int read = ins.read(buffer);
+            String answer = new String(buffer, 0, read);
+            System.out.println(answer);
+            
+            socket.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
--- src/com/v2soft/AndLib/ui/fragments/BluetoothDeviceList.java
+++ src/com/v2soft/AndLib/ui/fragments/BluetoothDeviceList.java
@@ -25,6 +25,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -45,8 +46,9 @@ import com.v2soft.AndLib.ui.loaders.BluetoothScannerLoader;
  */
 public abstract class BluetoothDeviceList<T extends BaseApplication<S>, S extends BaseApplicationSettings> 
 extends BaseFragment<T, S> 
-implements LoaderCallbacks<List<BluetoothDevice>>{
-    protected BluetoothDeviceAdapter mAdapter;
+implements LoaderCallbacks<List<BluetoothDevice>>, 
+    OnItemClickListener{
+    protected CustomViewAdapter<BluetoothDevice> mAdapter;
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -54,6 +56,7 @@ implements LoaderCallbacks<List<BluetoothDevice>>{
         View view = inflater.inflate(com.v2soft.AndLib.UI.R.layout.v2andlib_fragment_list, null);
         ListView list = (ListView) view.findViewById(android.R.id.list);
         mAdapter = new BluetoothDeviceAdapter(getActivity());
+        list.setOnItemClickListener(this);
         list.setAdapter(mAdapter);
         return view;
     }
