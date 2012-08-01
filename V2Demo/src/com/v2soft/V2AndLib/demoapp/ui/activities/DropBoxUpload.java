46392a6a49a531e749409f36fbf6e51668229409
Vladimir Shcriabets <vshcryabets@gmail.com>
2012-08-01 17:06:57 +0300

    - DropBox upload dialog mostly finished

--- V2Demo/src/com/v2soft/V2AndLib/demoapp/ui/activities/DropBoxUpload.java
+++ V2Demo/src/com/v2soft/V2AndLib/demoapp/ui/activities/DropBoxUpload.java
@@ -56,184 +56,33 @@ extends Activity implements OnClickListener {
     ///////////////////////////////////////////////////////////////////////////
     //                      Your app-specific settings.                      //
     ///////////////////////////////////////////////////////////////////////////
-
     // Replace this with your app key and secret assigned by Dropbox.
     // Note that this is a really insecure way to do this, and you shouldn't
     // ship code which contains your key & secret in such an obvious way.
     // Obfuscation is good.
     final static private String APP_KEY = "01ljvjlj0pbrow6";
     final static private String APP_SECRET = "yknmjmfogs868ad";
-
-    // If you'd like to change the access type to the full Dropbox instead of
-    // an app folder, change this value.
-    final static private AccessType ACCESS_TYPE = AccessType.APP_FOLDER;
-
     ///////////////////////////////////////////////////////////////////////////
     //                      End app-specific settings.                       //
     ///////////////////////////////////////////////////////////////////////////
-
-    // You don't need to change these, leave them alone.
-    final static private String ACCOUNT_PREFS_NAME = "prefs";
-    final static private String ACCESS_KEY_NAME = "ACCESS_KEY";
-    final static private String ACCESS_SECRET_NAME = "ACCESS_SECRET";
-    private static final String TAG = DropBoxUpload.class.getSimpleName();
-
-    private DropboxAPI<AndroidAuthSession> mApi;
-    private boolean mLoggedIn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_button);
-
-        // We create a new AuthSession so that we can use the Dropbox API.
-        AndroidAuthSession session = buildSession();
-        mApi = new DropboxAPI<AndroidAuthSession>(session);
-        checkAppKeySetup();
-
         findViewById(R.id.btnStart).setOnClickListener(this);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        AndroidAuthSession session = mApi.getSession();
-
-        // The next part must be inserted in the onResume() method of the
-        // activity from which session.startAuthentication() was called, so
-        // that Dropbox authentication completes properly.
-        if (session.authenticationSuccessful()) {
-            try {
-                // Mandatory call to complete the auth
-                session.finishAuthentication();
-
-                // Store it locally in our app for later use
-                TokenPair tokens = session.getAccessTokenPair();
-                storeKeys(tokens.key, tokens.secret);
-                setLoggedIn(true);
-            } catch (IllegalStateException e) {
-                showToast("Couldn't authenticate with Dropbox:" + e.getLocalizedMessage());
-                Log.i(TAG, "Error authenticating", e);
-            }
-        }
-    }    
-
-
-    private void logOut() {
-        // Remove credentials from the session
-        mApi.getSession().unlink();
-
-        // Clear our stored keys
-        clearKeys();
-        // Change UI state to display logged out version
-        setLoggedIn(false);
-    }
-
-    /**
-     * Convenience function to change UI state based on being logged in
-     */
-    private void setLoggedIn(boolean loggedIn) {
-        mLoggedIn = loggedIn;
-        //        if (loggedIn) {
-        //            mSubmit.setText("Unlink from Dropbox");
-        //            mDisplay.setVisibility(View.VISIBLE);
-        //        } else {
-        //            mSubmit.setText("Link with Dropbox");
-        //            mDisplay.setVisibility(View.GONE);
-        //            mImage.setImageDrawable(null);
-        //        }
-    }
-
-    private void checkAppKeySetup() {
-        // Check to make sure that we have a valid app key
-        if (APP_KEY.startsWith("CHANGE") ||
-                APP_SECRET.startsWith("CHANGE")) {
-            showToast("You must apply for an app key and secret from developers.dropbox.com, and add them to the DBRoulette ap before trying it.");
-            finish();
-            return;
-        }
-
-        // Check if the app has set up its manifest properly.
-        Intent testIntent = new Intent(Intent.ACTION_VIEW);
-        String scheme = "db-" + APP_KEY;
-        String uri = scheme + "://" + AuthActivity.AUTH_VERSION + "/test";
-        testIntent.setData(Uri.parse(uri));
-        PackageManager pm = getPackageManager();
-        if (0 == pm.queryIntentActivities(testIntent, 0).size()) {
-            showToast("URL scheme in your app's " +
-                    "manifest is not set up correctly. You should have a " +
-                    "com.dropbox.client2.android.AuthActivity with the " +
-                    "scheme: " + scheme);
-            finish();
-        }
-    }
-
-    private void showToast(String msg) {
-        Toast error = Toast.makeText(this, msg, Toast.LENGTH_LONG);
-        error.show();
-    }
-
-    /**
-     * Shows keeping the access keys returned from Trusted Authenticator in a local
-     * store, rather than storing user name & password, and re-authenticating each
-     * time (which is not to be done, ever).
-     *
-     * @return Array of [access_key, access_secret], or null if none stored
-     */
-    private String[] getKeys() {
-        SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
-        String key = prefs.getString(ACCESS_KEY_NAME, null);
-        String secret = prefs.getString(ACCESS_SECRET_NAME, null);
-        if (key != null && secret != null) {
-            String[] ret = new String[2];
-            ret[0] = key;
-            ret[1] = secret;
-            return ret;
-        } else {
-            return null;
-        }
-    }
-
-    /**
-     * Shows keeping the access keys returned from Trusted Authenticator in a local
-     * store, rather than storing user name & password, and re-authenticating each
-     * time (which is not to be done, ever).
-     */
-    private void storeKeys(String key, String secret) {
-        // Save the access key for later
-        SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
-        Editor edit = prefs.edit();
-        edit.putString(ACCESS_KEY_NAME, key);
-        edit.putString(ACCESS_SECRET_NAME, secret);
-        edit.commit();
-    }
-
-    private void clearKeys() {
-        SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
-        Editor edit = prefs.edit();
-        edit.clear();
-        edit.commit();
-    }
-
-    private AndroidAuthSession buildSession() {
-        AppKeyPair appKeyPair = new AppKeyPair(APP_KEY, APP_SECRET);
-        AndroidAuthSession session;
-
-        String[] stored = getKeys();
-        if (stored != null) {
-            AccessTokenPair accessToken = new AccessTokenPair(stored[0], stored[1]);
-            session = new AndroidAuthSession(appKeyPair, ACCESS_TYPE, accessToken);
-        } else {
-            session = new AndroidAuthSession(appKeyPair, ACCESS_TYPE);
-        }
-
-        return session;
-    }    
-
-    @Override
     public void onClick(View v) {
-        DropboxUploadFragment frag = DropboxUploadFragment.newInstance("/sdcard/cloc", "cloc");
-        frag.show(getFragmentManager(), "dialog");
+        try {
+            DropboxUploadFragment frag;
+            frag = DropboxUploadFragment.newInstance(this, "/sdcard/cloc", "cloc",
+                    APP_KEY, APP_SECRET);
+            frag.show(getFragmentManager(), "dialog");
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 /*        if (!mLoggedIn) {
             // Start the remote authentication
             mApi.getSession().startAuthentication(this);
