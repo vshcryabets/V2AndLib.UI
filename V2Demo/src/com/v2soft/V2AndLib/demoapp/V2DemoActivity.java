01c837973d7f01a7360f1e809d3c55aea66bd405
V.Shcryabets <vshcryabets@gmail.com>
2012-08-12 20:21:03 +0300

    - Google Cloud Print sample

--- V2Demo/src/com/v2soft/V2AndLib/demoapp/V2DemoActivity.java
+++ V2Demo/src/com/v2soft/V2AndLib/demoapp/V2DemoActivity.java
@@ -17,6 +17,7 @@ package com.v2soft.V2AndLib.demoapp;
 
 import com.v2soft.V2AndLib.demoapp.ui.activities.BluetoothList;
 import com.v2soft.V2AndLib.demoapp.ui.activities.DropBoxUpload;
+import com.v2soft.V2AndLib.demoapp.ui.activities.GCPDemo;
 import com.v2soft.V2AndLib.demoapp.ui.activities.OpenSLSample;
 import com.v2soft.V2AndLib.demoapp.ui.activities.UDPDiscoveryList;
 
@@ -40,12 +41,14 @@ implements OnItemClickListener {
     private final static String [] sItems = new String[]{"Bluetooth device list",
         "UDP discovery",
         "Upload to dropbox",
-        "OpenSL sample"};
+        "OpenSL sample",
+        "Google Cloud Printing Demo"};
     private static final String LOG_TAG = V2DemoActivity.class.getSimpleName();
     private static final int ITEM_BLUETOOTH_DEVICE = 0;
     private static final int ITEM_UDP_DISCOVERY = 1;
     private static final int ITEM_DROPBOX_UPLOAD = 2;
     private static final int ITEM_OPEN_SL = 3;
+    private static final int ITEM_GCP = 4;
     
     private ArrayAdapter<String> mAdapter;
     
@@ -73,6 +76,9 @@ implements OnItemClickListener {
             break;
         case ITEM_OPEN_SL:
             startActivity(new Intent(this, OpenSLSample.class));
+            break;
+        case ITEM_GCP:
+            startActivity(new Intent(this, GCPDemo.class));
             break;
         default:
             break;
---
+++ V2Demo/src/com/v2soft/V2AndLib/demoapp/ui/activities/GCPDemo.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2012 V.Shcryabets (vshcryabets@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.v2soft.V2AndLib.demoapp.ui.activities;
+
+import java.io.File;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.v2soft.AndLib.ui.R;
+
+/**
+ * Google Cloud printing sample
+ * @author V.Shcriyabets (vshcryabets@gmail.com)
+ *
+ */
+public class GCPDemo 
+extends Activity implements OnClickListener {
+	private static final String TAG = GCPDemo.class.getSimpleName();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fragment_button);
+		findViewById(R.id.btnStart).setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		Intent printIntent = new Intent(this, PrintDialogActivity.class);
+		printIntent.setDataAndType(Uri.fromFile(new File("/sdcard/test.jpeg")), "image/jpeg");
+		printIntent.putExtra("title", "Photo test");
+		startActivity(printIntent);
+	}
+
+
+}
