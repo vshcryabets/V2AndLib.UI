1d8ce6d3cb7e8f55f6312c6e8c74a596e6b978cd
Vladimir Shcriabets <vshcryabets@gmail.com>
2012-08-27 13:03:22 +0300

    DataContainer class for loaders

---
+++ src/com/v2soft/AndLib/dao/DataContainer.java
@@ -0,0 +1,25 @@
+package com.v2soft.AndLib.dao;
+
+
+/**
+ * 
+ * @author V.Shcriyabets (vshcryabets@gmail.com)
+ *
+ */
+public class DataContainer<T> {
+    protected T mData;
+    protected Throwable mError;
+    
+    public DataContainer(T data) {
+        setData(data);
+    }
+    public DataContainer(Throwable error) {
+        mError = error;
+    }
+    
+    public T getData() {return mData;}
+    public Throwable getError() {return mError;}
+    public boolean isSuccesfull(){return mError==null;}
+    public void setData(T data) {mData = data;}
+    public void setError(Throwable data) {mError = data;}
+}
