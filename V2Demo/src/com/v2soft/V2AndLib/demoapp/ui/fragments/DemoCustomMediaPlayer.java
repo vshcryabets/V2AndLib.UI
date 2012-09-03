426df321f906795208010f352b1a7c395a5d4584
V.Shcryabets <vshcryabets@gmail.com>
2012-09-03 11:18:08 +0300

    - demo for CustomMediaPlayer

--- V2Demo/src/com/v2soft/V2AndLib/demoapp/ui/fragments/DemoCustomMediaPlayer.java
+++ V2Demo/src/com/v2soft/V2AndLib/demoapp/ui/fragments/DemoCustomMediaPlayer.java
@@ -15,21 +15,14 @@
  */
 package com.v2soft.V2AndLib.demoapp.ui.fragments;
 
-import java.util.LinkedList;
-import java.util.List;
-
-import android.content.Context;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import com.v2soft.AndLib.ui.Adapters.BackLoadingAdapter;
 import com.v2soft.AndLib.ui.fragments.BaseFragment;
-import com.v2soft.AndLib.ui.views.IDataView;
 import com.v2soft.V2AndLib.demoapp.DemoAppSettings;
 import com.v2soft.V2AndLib.demoapp.DemoApplication;
 
@@ -40,8 +33,6 @@ import com.v2soft.V2AndLib.demoapp.DemoApplication;
  */
 public class DemoCustomMediaPlayer 
 extends BaseFragment<DemoApplication, DemoAppSettings>  {
-    private EndlessAdapter mAdapter;
-
     public static Fragment newInstance() {
         return new DemoCustomMediaPlayer();
     }
@@ -51,61 +42,10 @@ extends BaseFragment<DemoApplication, DemoAppSettings>  {
             Bundle savedInstanceState) {
         final View view = inflater.inflate(com.v2soft.AndLib.ui.R.layout.v2andlib_fragment_list, null);
         ListView list = (ListView) view.findViewById(android.R.id.list);
-        mAdapter = new EndlessAdapter(getActivity());
-        list.setAdapter(mAdapter);
         return view;
     }
 
     @Override
     public void onClick(View v) {
-    }
-
-    private class IntegerView extends TextView implements IDataView<Integer> {
-        private Integer mData;
-        public IntegerView(Context context) {
-            super(context);
-        }
-
-        @Override
-        public void setData(Integer data) {
-            mData = data;
-            setText(String.valueOf(data));
-        }
-
-        @Override
-        public Integer getData() {
-            return mData;
-        }
-
-    }
-
-    private class EndlessAdapter extends BackLoadingAdapter<Integer> {
-
-        public EndlessAdapter(Context context) {
-            super(context, new CustomViewAdapterFactory<Integer, IDataView<Integer>>(){
-                @Override
-                public IDataView<Integer> createView(Context context, int type) {
-                    return new IntegerView(context);
-                }
-            }, 10);
-        }
-
-        @Override
-        protected List<Integer> getData(int start, int count) {
-            List<Integer> result = new LinkedList<Integer>();
-            for ( int i = 0; i< count; i++ ) {
-                if ( start+i > 100 ) {
-                    break;
-                }
-                result.add(start+i);
-            }
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            return result;
-        }
     }
 }
