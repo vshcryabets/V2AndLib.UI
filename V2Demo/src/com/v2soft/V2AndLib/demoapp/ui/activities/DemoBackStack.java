d3aac291db8b86c0483040ab16c3ff8673d87f24
Vladimir Shcriabets <vshcryabets@gmail.com>
2012-09-03 17:52:25 +0300

    - back stack demo

---
+++ V2Demo/src/com/v2soft/V2AndLib/demoapp/ui/activities/DemoBackStack.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2012 V.Shcryabets (vshcryabets@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.v2soft.V2AndLib.demoapp.ui.activities;
+
+import com.v2soft.AndLib.ui.R;
+import com.v2soft.V2AndLib.demoapp.ui.fragments.DemoBluetoothDeviceList;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+
+/**
+ * 
+ * @author vshcryabets@gmail.com
+ *
+ */
+public class DemoBackStack extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.v2andlib_fragment_tabs);
+    }
+}
