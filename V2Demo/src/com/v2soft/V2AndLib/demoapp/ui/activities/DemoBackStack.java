c5a713d82b16e3ecb07e64bb429da3db8909ae08
Vladimir Shcriabets <vshcryabets@gmail.com>
2012-09-05 16:15:09 +0300

    - tab back stack - tetsing save/restore state

--- V2Demo/src/com/v2soft/V2AndLib/demoapp/ui/activities/DemoBackStack.java
+++ V2Demo/src/com/v2soft/V2AndLib/demoapp/ui/activities/DemoBackStack.java
@@ -15,29 +15,28 @@
  */
 package com.v2soft.V2AndLib.demoapp.ui.activities;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.v2soft.AndLib.ui.R;
-import com.v2soft.AndLib.ui.fragments.BaseFragment;
+import com.v2soft.AndLib.ui.activities.BaseActivity;
 import com.v2soft.AndLib.ui.fragments.TabsFragmentBackStack;
 import com.v2soft.AndLib.ui.fragments.TabsFragmentBackStack.TabsFragmentBackStackListener;
 import com.v2soft.V2AndLib.demoapp.DemoAppSettings;
 import com.v2soft.V2AndLib.demoapp.DemoApplication;
+import com.v2soft.V2AndLib.demoapp.ui.fragments.TextButtonFragment;
 
 /**
  * 
  * @author vshcryabets@gmail.com
  *
  */
-public class DemoBackStack extends Activity implements OnClickListener {
+public class DemoBackStack 
+extends BaseActivity<DemoApplication, DemoAppSettings> 
+implements OnClickListener {
     private static final String TAB_A = "A";
     private static final String TAB_B = "B";
     private static final String TAB_C = "C";
@@ -53,7 +52,10 @@ public class DemoBackStack extends Activity implements OnClickListener {
         findViewById(R.id.btn3).setOnClickListener(this);
         findViewById(R.id.btn4).setOnClickListener(this);
         mStack = new TabsFragmentBackStack(mFragmentsListener);
-        if ( savedInstanceState == null ) {
+        mCustomStack = mStack;
+        if ( savedInstanceState != null ) {
+            mStack.onRestoreInstanceState(savedInstanceState);
+        } else {
             mStack.activateTab(TAB_A);
         }
     }
@@ -62,13 +64,13 @@ public class DemoBackStack extends Activity implements OnClickListener {
         @Override
         public Fragment onNewTabOpened(String tag) {
             if ( tag.equals(TAB_A)) {
-                return newInstance(TAB_A,1);
+                return TextButtonFragment.newInstance(TAB_A,1);
             } else if ( tag.equals(TAB_B)) {
-                return newInstance(TAB_B,1);
+                return TextButtonFragment.newInstance(TAB_B,1);
             } else if ( tag.equals(TAB_C)) {
-                return newInstance(TAB_C,1);
+                return TextButtonFragment.newInstance(TAB_C,1);
             } else if ( tag.equals(TAB_D)) {
-                return newInstance(TAB_D,1);
+                return TextButtonFragment.newInstance(TAB_D,1);
             }
             return null;
         }
@@ -127,66 +129,4 @@ public class DemoBackStack extends Activity implements OnClickListener {
         }
     }
 
-    public Fragment newInstance(String text, int id) {
-        final Fragment result = new TextFragment();
-        final Bundle args = new Bundle();
-        args.putString(TextFragment.KEY_STR, text);
-        args.putInt(TextFragment.KEY_ID, id);
-        result.setArguments(args);
-        return result;
-    }
-
-    private class TextFragment 
-    extends BaseFragment<DemoApplication, DemoAppSettings> {
-        public static final String KEY_ID = "id";
-        private static final String KEY_STR = "str";
-        private String mTag;
-        private int mId;
-
-        public Fragment newInstance(String text, int id) {
-            final Fragment result = new TextFragment();
-            final Bundle args = new Bundle();
-            args.putString(KEY_STR, text);
-            args.putInt(KEY_ID, id);
-            result.setArguments(args);
-            return result;
-        }
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            mTag = getArguments().getString(KEY_STR);
-            mId = getArguments().getInt(KEY_ID);
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            final View view = inflater.inflate(R.layout.fragment_button, null);
-            final Button btn = (Button) view.findViewById(R.id.btnStart);
-            btn.setOnClickListener(this);
-            btn.setText(mTag+"."+mId);
-            return view;
-        }
-        @Override
-        public void onClick(View v) {
-            Fragment fragment = null;
-            if ( mId > 4 ) {
-                String next = TAB_A;
-                if ( mTag.equals(TAB_A)) {
-                    next = TAB_B;
-                } else if ( mTag.equals(TAB_B)) {
-                    next = TAB_C;
-                } else if ( mTag.equals(TAB_C)) {
-                    next = TAB_D;
-                }
-                fragment = newInstance(next, 1);
-                mStack.startFragment(fragment, next);
-            } else {
-                fragment = newInstance(mTag, ++mId);
-                mStack.startFragmentInCurrentTab(fragment);
-            }
-
-        }
-    }
 }
--- src/com/v2soft/AndLib/ui/activities/BaseActivity.java
+++ src/com/v2soft/AndLib/ui/activities/BaseActivity.java
@@ -1,6 +1,7 @@
 package com.v2soft.AndLib.ui.activities;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 
 import com.v2soft.AndLib.application.BaseApplication;
@@ -15,8 +16,22 @@ import com.v2soft.AndLib.application.BaseApplicationSettings;
  */
 public abstract class BaseActivity<T extends BaseApplication<S>, S extends BaseApplicationSettings> 
 extends Activity {
+    // =================================================================
+    // Interfaces
+    // =================================================================
+    public interface IBackStack {
+        void startFragmentAt(Fragment fragment, String where);
+        void startFragment(Fragment fragment);
+        boolean onBackPressed();
+        void onSaveInstanceState(Bundle outState);
+        void onRestoreInstanceState(Bundle state);
+    }
+    // =================================================================
+    // Class fields
+    // =================================================================
     protected T mApp;
     protected S mSettings;
+    protected IBackStack mCustomStack;
 
     @SuppressWarnings("unchecked")
     @Override
@@ -30,5 +45,17 @@ extends Activity {
         if ( mSettings == null ) {
             throw new NullPointerException("Settings is null");
         }
+    }
+    
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if ( mCustomStack != null ) {
+            mCustomStack.onSaveInstanceState(outState);
+        }
+    }
+    
+    public IBackStack getBackStack() {
+        return mCustomStack;
     }
 }
