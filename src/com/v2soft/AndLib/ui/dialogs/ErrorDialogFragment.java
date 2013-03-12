0142c02db53fe0160606ce53fb1a027a184426eb
V.Shcryabets <vshcryabets@gmail.com>
2013-03-12 12:49:07 +0200

    Backport to API10

--- src/com/v2soft/AndLib/ui/dialogs/ErrorDialogFragment.java
+++ src/com/v2soft/AndLib/ui/dialogs/ErrorDialogFragment.java
@@ -17,8 +17,8 @@ package com.v2soft.AndLib.ui.dialogs;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 
 import com.v2soft.AndLib.ui.R;
 
