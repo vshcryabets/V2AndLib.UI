39f69c6f4634ae921814da064b356b40f35269e8
V.Shcryabets <vshcryabets@gmail.com>
2013-05-29 16:34:26 +0300

    BaseActivity can return casted application class

--- src/com/v2soft/AndLib/ui/activities/BaseActivity.java
+++ src/com/v2soft/AndLib/ui/activities/BaseActivity.java
@@ -17,7 +17,7 @@ import com.v2soft.AndLib.ui.fonts.FontManager;
  * @param <S> settings class
  */
 public abstract class BaseActivity<T extends BaseApplication<S>, S extends BaseApplicationSettings> 
-extends SherlockFragmentActivity implements IBaseActivity {
+extends SherlockFragmentActivity implements IBaseActivity<T> {
     // =================================================================
     // Interfaces
     // =================================================================
