1580cae1c0051dbe78d3c7143bf34cbec3497d3a
V.Shcryabets <vshcryabets@gmail.com>
2013-07-31 10:23:14 +0300

    Added AbstractCommunicationComment class

--- src/com/v2soft/AndLib/communications/AbstractCommunicationMessage.java
+++ src/com/v2soft/AndLib/communications/AbstractCommunicationMessage.java
@@ -9,73 +9,22 @@ import java.io.Serializable;
  * @param <U> communication user data type
  * @param <ID> message ID
  */
-public class AbstractCommunicationMessage<U extends AbstractCommunicationUser<?>, MID> 
+public class AbstractCommunicationMessage<U extends AbstractCommunicationUser<?>, MID>
+    extends AbstractCommunicationComment<U, MID>
     implements Serializable {
     private static final long serialVersionUID = 2L;
-    protected MID mId;
-    protected U mSender;
     protected U mRecepient;
-    protected boolean isRead;
     
     public AbstractCommunicationMessage() {
         isRead = false;
     }
     /**
-     * @return the mId
-     */
-    public MID getId() {
-        return mId;
-    }
-    /**
-     * @return the mSender
-     */
-    public U getSender() {
-        return mSender;
-    }
-    /**
-     * @return the mRecepient
+     * Return message recipient.
+     * @return message recipient.
      */
     public U getRecepient() {
         return mRecepient;
     }
-    /**
-     * @param mId the mId to set
-     */
-    public void setId(MID mId) {
-        this.mId = mId;
-    }
-    /**
-     * @param mSender the mSender to set
-     */
-    public void setSender(U mSender) {
-        this.mSender = mSender;
-    }
-    /**
-     * @param mRecepient the mRecepient to set
-     */
-    public void setRecepient(U mRecepient) {
-        this.mRecepient = mRecepient;
-    }
-    /**
-     * 
-     * @return true if this message marked as read
-     */
-    public boolean isRead() {
-        return isRead;
-    }
-    /**
-     * 
-     * @param isRead should be true if this message marked as read
-     */
-    protected void setRead(boolean isRead) {
-        this.isRead = isRead;
-    };
-    
-    @Override
-    public int hashCode() {
-        return mId.hashCode();
-    }
-    
     public boolean equals(Object o) {
         if ( o == null ) {
             return false;
@@ -86,5 +35,4 @@ public class AbstractCommunicationMessage<U extends AbstractCommunicationUser<?>
         }
         return false;
     }
-
 }
