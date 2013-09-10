e591c7b5e68e18a541df26a95ae4a8fe3ddfd3c8
V.Shcryabets <vshcryabets@gmail.com>
2013-09-10 14:28:24 +0300

    Prepared video recording routines in CameraView

--- V2Demo/src/com/v2soft/V2AndLib/demoapp/ui/fragments/DemoCamera.java
+++ V2Demo/src/com/v2soft/V2AndLib/demoapp/ui/fragments/DemoCamera.java
@@ -15,9 +15,11 @@
  */
 package com.v2soft.V2AndLib.demoapp.ui.fragments;
 
+import java.io.File;
 import java.util.UUID;
 
 import android.app.Fragment;
+import android.media.CameraProfile;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -72,7 +74,17 @@ extends BaseFragment<DemoApplication, DemoAppSettings>  {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if ( item.getItemId() == R.id.menu_take_video ) {
-            mCameraView.startVideoRecording(getActivity().getExternalCacheDir()+UUID.randomUUID().toString());
+            try {
+                if ( mCameraView.isRecording() ) {
+                    mCameraView.stopRecording();
+                } else {
+                    mCameraView.startVideoRecording(getActivity().getExternalCacheDir()+File.separator+
+                            UUID.randomUUID().toString(), CameraProfile.QUALITY_HIGH);
+                }
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         } else if ( item.getItemId() == R.id.menu_take_photo ) {
             //mCameraView.takePicture(path, portrait, flash_disable);
         }
