58dc675831afad83f554a6a558d9ccf94aaf726c
V.Shcryabets <vshcryabets@gmail.com>
2014-03-10 23:09:33 +0200

    PCM sine tone generator for unit tests

--- V2Demo/instrumentTest/java/com/v2soft/AndLib/ui/test/AudioOperationsTests.java
+++ V2Demo/instrumentTest/java/com/v2soft/AndLib/ui/test/AudioOperationsTests.java
@@ -10,6 +10,7 @@ import com.v2soft.AndLib.filecache.FileCache;
 import com.v2soft.AndLib.media.MP3EncodingOutputStream;
 import com.v2soft.AndLib.media.MP3Helper;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URI;
@@ -38,6 +39,40 @@ public class AudioOperationsTests extends AndroidTestCase {
                 URI.create("file://" + cache.getCachePathURI(URI.create(ASSET_SOURCE_FILE_PATH))));
 		assertTrue("Zero size output file", outWrapper.getAvaiableDataSize() > 0 );
 		assertTrue("Wrong compression", wrapper.getAvaiableDataSize() >= outWrapper.getAvaiableDataSize() );
+
+        // generate sine and write it to file
+        byte[] data = sineGenerator(true, 44100, 3000, 4600);
+        String path = cache.getCachePathURI(URI.create("local:///sineData4600"));
+        FileOutputStream out = cache.getFileOutputStream(URI.create("local:///sineData4600"));
+        out.write(data);
+        out.close();
+        // encode data from file
+        // decode from MP3 file
+        // check PCM data
 	}
 
+    private byte[] sineGenerator(boolean stereo, int sampleRate, int longInMs, int frequencyInHZ) {
+        int framesCount = sampleRate * longInMs / 1000;
+        int bytesPerFrame = (stereo ? 4 : 2);
+        byte dataArray[] = new byte[framesCount*bytesPerFrame];
+        double left = 0.0;
+        double right = Math.PI/2;
+        double scale = frequencyInHZ/(1/(2*Math.PI));
+        for ( int i = 0; i < framesCount; i++ ) {
+            double t = (double)i / (double)sampleRate;
+            short leftSample = (short) (Short.MAX_VALUE*Math.sin(t*scale+left));
+            short rightSample = (short) (Short.MAX_VALUE*Math.sin(t*scale+right));
+            byte first = (byte) (leftSample >> 8);
+            byte second = (byte) (leftSample & 0xFF);
+            dataArray[i*bytesPerFrame] = first;
+            dataArray[i*bytesPerFrame+1] = second;
+            if ( stereo ) {
+                first = (byte) (rightSample >> 8);
+                second = (byte) (rightSample & 0xFF);
+                dataArray[i * bytesPerFrame + 2] = first;
+                dataArray[i * bytesPerFrame + 3] = second;
+            }
+        }
+        return dataArray;
+    }
 }
