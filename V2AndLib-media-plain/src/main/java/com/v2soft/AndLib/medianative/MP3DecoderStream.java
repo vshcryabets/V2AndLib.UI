7a9ffe4005957105c8ad3fae838b6e53280a6059
Vladimir Shcryabets <vshcryabets@gmail.com>
2014-09-25 10:14:06 +0300

    Fixed libs path in gradle build scripts

--- V2AndLib-media-plain/src/main/java/com/v2soft/AndLib/medianative/MP3DecoderStream.java
+++ V2AndLib-media-plain/src/main/java/com/v2soft/AndLib/medianative/MP3DecoderStream.java
@@ -9,19 +9,9 @@ import java.io.InputStream;
  * @author V.Shcryabets (vshcryabets@gmail.com)
  */
 public class MP3DecoderStream extends InputStream {
+
     static {
-        System.out.println(System.getProperty("java.library.path"));
-        String f = null;
-	if (System.getProperty("sun.arch.data.model").equals("32")) {
-	    // 32-bit JVM
-	    f = "audiostreams32";
-	} else {
-	    // 64-bit JVM
-	    f = "audiostreams64";
-	}
-	System.out.println(f);
-        //System.loadLibrary(f);
-        System.load("/Users/vshcryabets/Documents/git/V2AndLib.UI/V2AndLib-media-plain/libs/host/libaudiostreams64.so");
+        System.loadLibrary("audiostreams");
     }
 
     private int mHandlerId = 0;
