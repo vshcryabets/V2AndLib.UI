1ddeea9e0c013772193ad53f3d63258b69b6123a
Vladimir Shcryabets <vshcryabets@gmail.com>
2015-02-10 07:01:50 +0200

    Updated BitmapOperations tests

--- V2AndLib-media/src/com/v2soft/AndLib/media/BitmapOperations.java
+++ V2AndLib-media/src/com/v2soft/AndLib/media/BitmapOperations.java
@@ -28,97 +28,107 @@ import java.net.URI;
 
 /**
  * Bitmap operations helper.
+ *
  * @author V.Shcriyabets (vshcryabets@gmail.com)
  */
 public class BitmapOperations {
-    public enum ScaleType {CENTER_CROP,
+    public enum ScaleType {
+        CENTER_CROP,
         FIT_CENTER,
         SCALE_HORIZONTAL,
         SCALE_VERTICAL,
-        NONE}
-	protected Context mContext;
+        NONE
+    }
 
-	public BitmapOperations(Context context) {
-		mContext = context;
-	}
+    protected Context mContext;
 
-	/**
-	 * Load bitmap options.
-	 * @param uri bitmap source uri.
-	 * @return bitmap options.
-	 * @throws IOException
-	 */
-	public BitmapFactory.Options getBitmapOptions(URI uri) throws IOException {
-		BitmapFactory.Options options = new BitmapFactory.Options();
-		options.inJustDecodeBounds = true;
-		loadBitmap(uri, options);
-		return options;
-	}
+    public BitmapOperations(Context context) {
+        mContext = context;
+    }
 
-	/**
-	 * Load bitmap from specified source.
-	 * @param uri bitmap source uri.
-	 * @param options
-	 * @return bitmap object.
-	 */
-	public Bitmap loadBitmap(URI uri, BitmapFactory.Options options) throws IOException {
-		StreamHelper stream = AndroidStreamHelper.getStream(mContext, uri);
-		Bitmap result = BitmapFactory.decodeStream(stream.getInputStream(), null, options);
-		stream.close();
-		return result;
-	}
+    /**
+     * Load bitmap options.
+     *
+     * @param uri bitmap source uri.
+     * @return bitmap options.
+     * @throws IOException
+     */
+    public BitmapFactory.Options getBitmapOptions(URI uri) throws IOException {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        loadBitmap(uri, options);
+        return options;
+    }
 
-	/**
-	 * Load bitmap from specified source.
-	 * @param uri bitmap source uri.
-	 * @return bitmap from specified source.
-	 */
-	public Bitmap loadBitmap(URI uri) throws IOException {
-		return loadBitmap(uri, null);
-	}
-	/**
-	 * Load bitmap from specified source.
-	 * @param uri bitmap source uri.
-	 * @param margins destination rectangle.
-	 * @param scaleType scale type.
-	 * @param filter    true if the source should be filtered.
-	 * @return bitmap from specified source.
-	 */
-	public Bitmap loadBitmap(URI uri, Rect margins, ScaleType scaleType, boolean filter) throws IOException {
-		BitmapFactory.Options options = getBitmapOptions(uri);
-		Rect destRect = scaleRect(options, margins, scaleType);
-		options = getBitmapScaleOptions(uri, destRect, scaleType);
-		Bitmap bitmap = loadBitmap(uri, options);
-        if ( bitmap != null ) {
+    /**
+     * Load bitmap from specified source.
+     *
+     * @param uri     bitmap source uri.
+     * @param options
+     * @return bitmap object.
+     */
+    public Bitmap loadBitmap(URI uri, BitmapFactory.Options options) throws IOException {
+        StreamHelper stream = AndroidStreamHelper.getStream(mContext, uri);
+        Bitmap result = BitmapFactory.decodeStream(stream.getInputStream(), null, options);
+        stream.close();
+        return result;
+    }
+
+    /**
+     * Load bitmap from specified source.
+     *
+     * @param uri bitmap source uri.
+     * @return bitmap from specified source.
+     */
+    public Bitmap loadBitmap(URI uri) throws IOException {
+        return loadBitmap(uri, null);
+    }
+
+    /**
+     * Load bitmap from specified source.
+     *
+     * @param uri       bitmap source uri.
+     * @param margins   destination rectangle.
+     * @param scaleType scale type.
+     * @param filter    true if the source should be filtered.
+     * @return bitmap from specified source.
+     */
+    public Bitmap loadBitmap(URI uri, Rect margins, ScaleType scaleType, boolean filter) throws IOException {
+        BitmapFactory.Options options = getBitmapOptions(uri);
+        Rect destRect = scaleRect(options, margins, scaleType);
+        options = getBitmapScaleOptions(uri, destRect, scaleType);
+        Bitmap bitmap = loadBitmap(uri, options);
+        if (bitmap != null) {
             Bitmap result = Bitmap.createScaledBitmap(bitmap, destRect.width(), destRect.height(), filter);
-            if ( result != bitmap ) {
+            if (!result.equals(bitmap)) {
                 bitmap.recycle();
             }
             return result;
         } else {
             throw new IOException("Can't read bitmap");
         }
-	}
+    }
 
-	/**
-	 * Calculate scale factor.
-	 * @param scaleType scale type
-	 * @param destRect margins.
-	 * @param sourceRect source bitmap rectangle.
-	 * @return scale factor.
-	 */
+    /**
+     * Calculate scale factor.
+     *
+     * @param scaleType  scale type
+     * @param destRect   margins.
+     * @param sourceRect source bitmap rectangle.
+     * @return scale factor.
+     */
     public float getScaleFactor(ScaleType scaleType, Rect destRect, Rect sourceRect) {
-        float horizontalScaleFactor = (float)destRect.width()/(float)sourceRect.width();
-        float verticalScaleFactor = (float)destRect.height()/(float)sourceRect.height();
+        float horizontalScaleFactor = (float) destRect.width() / (float) sourceRect.width();
+        float verticalScaleFactor = (float) destRect.height() / (float) sourceRect.height();
         float maxFactor = Math.max(verticalScaleFactor, horizontalScaleFactor);
         float minFactor = Math.min(verticalScaleFactor, horizontalScaleFactor);
-        if ( scaleType == ScaleType.FIT_CENTER ) {
+        if (scaleType == ScaleType.FIT_CENTER) {
             return minFactor;
-        } else if ( scaleType == ScaleType.CENTER_CROP ) {
+        } else if (scaleType == ScaleType.CENTER_CROP) {
             return maxFactor;
-        } else if ( scaleType == ScaleType.SCALE_HORIZONTAL ) {
+        } else if (scaleType == ScaleType.SCALE_HORIZONTAL) {
             return horizontalScaleFactor;
-        } else if ( scaleType == ScaleType.SCALE_VERTICAL ) {
+        } else if (scaleType == ScaleType.SCALE_VERTICAL) {
             return verticalScaleFactor;
         }
         return 1.0f;
@@ -126,54 +136,58 @@ public class BitmapOperations {
 
 
     /**
-	 * Calculate BitmapFactory.Options::inScaleSize value to fit bitmap in specified margins.
-	 * @param uri bitmap source uri.
-	 * @param destRect destination margins.
-	 * @param scaleType scale type.
-	 * @return bitmap loading options.
-	 */
-	public BitmapFactory.Options getBitmapScaleOptions(URI uri, Rect destRect, ScaleType scaleType) throws IOException {
-		BitmapFactory.Options options = getBitmapOptions(uri);
-		Rect srcRect = new Rect(0,0, options.outWidth, options.outHeight);
-		float scaleDownFactor = 1/getScaleFactor(scaleType, destRect, srcRect);
-		options = new BitmapFactory.Options();
-		double scaleDownPower2 = Math.log(scaleDownFactor)/Math.log(2);
-		scaleDownPower2 = Math.floor(scaleDownPower2);
-		int scaleDownInt = (int) Math.pow(2, scaleDownPower2);
-		options.inSampleSize = scaleDownInt;
-		return options;
-	}
+     * Calculate BitmapFactory.Options::inScaleSize value to fit bitmap in specified margins.
+     *
+     * @param uri       bitmap source uri.
+     * @param destRect  destination margins.
+     * @param scaleType scale type.
+     * @return bitmap loading options.
+     */
+    public BitmapFactory.Options getBitmapScaleOptions(URI uri, Rect destRect, ScaleType scaleType) throws IOException {
+        BitmapFactory.Options options = getBitmapOptions(uri);
+        Rect srcRect = new Rect(0, 0, options.outWidth, options.outHeight);
+        float scaleDownFactor = 1 / getScaleFactor(scaleType, destRect, srcRect);
+        options = new BitmapFactory.Options();
+        double scaleDownPower2 = Math.log(scaleDownFactor) / Math.log(2);
+        scaleDownPower2 = Math.floor(scaleDownPower2);
+        int scaleDownInt = (int) Math.pow(2, scaleDownPower2);
+        options.inSampleSize = scaleDownInt;
+        return options;
+    }
 
-	/**
-	 * Scale bitmap to fit inside specified margins.
-	 * @param bitmap source bitmap.
-	 * @param margins margins.
-	 * @return scaled bitmap object.
-	 */
-	public Bitmap scaleBitmap(Bitmap bitmap, Rect margins, ScaleType scaleType, boolean filter) {
-		Rect rect = scaleRect(bitmap, margins, scaleType);
-		Bitmap result = Bitmap.createScaledBitmap(bitmap, rect.width(), rect.height(), filter);
-		return result;
-	}
+    /**
+     * Scale bitmap to fit inside specified margins.
+     *
+     * @param bitmap  source bitmap.
+     * @param margins margins.
+     * @return scaled bitmap object.
+     */
+    public Bitmap scaleBitmap(Bitmap bitmap, Rect margins, ScaleType scaleType, boolean filter) {
+        Rect rect = scaleRect(bitmap, margins, scaleType);
+        Bitmap result = Bitmap.createScaledBitmap(bitmap, rect.width(), rect.height(), filter);
+        return result;
+    }
 
-	protected Rect scaleRect(Bitmap bitmap, Rect margins, ScaleType type) {
-		Rect source = new Rect(0,0, bitmap.getWidth(), bitmap.getHeight());
-		return scaleRect(source, margins, type);
-	}
-	protected Rect scaleRect(BitmapFactory.Options options, Rect margins, ScaleType scaleType) {
-		Rect srcRect = new Rect(0,0, options.outWidth, options.outHeight);
-		return scaleRect(srcRect, margins, scaleType);
-	}
-	protected Rect scaleRect(Rect source, Rect margins, ScaleType type) {
-		float scaleDownFactor = getScaleFactor(type, margins, source);
-		return multipleRect(source, scaleDownFactor);
-	}
+    protected Rect scaleRect(Bitmap bitmap, Rect margins, ScaleType type) {
+        Rect source = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        return scaleRect(source, margins, type);
+    }
 
-	protected Rect multipleRect(Rect src, float factor) {
-		return new Rect((int)(src.left*factor),
-				(int)(src.top*factor),
-				(int)(src.right*factor),
-				(int)(src.bottom*factor));
-	}
+    protected Rect scaleRect(BitmapFactory.Options options, Rect margins, ScaleType scaleType) {
+        Rect srcRect = new Rect(0, 0, options.outWidth, options.outHeight);
+        return scaleRect(srcRect, margins, scaleType);
+    }
+
+    protected Rect scaleRect(Rect source, Rect margins, ScaleType type) {
+        float scaleDownFactor = getScaleFactor(type, margins, source);
+        return multipleRect(source, scaleDownFactor);
+    }
+
+    protected Rect multipleRect(Rect src, float factor) {
+        return new Rect((int) (src.left * factor),
+                (int) (src.top * factor),
+                (int) (src.right * factor),
+                (int) (src.bottom * factor));
+    }
 
 }
--- V2Demo/src/androidTest/java/com/v2soft/AndLib/demotest/BitmapOperationsTests.java
+++ V2Demo/src/androidTest/java/com/v2soft/AndLib/demotest/BitmapOperationsTests.java
@@ -15,77 +15,79 @@ import java.net.URI;
  * @author Vladimir Shcryabets <vshcryabets@gmail.com>
  */
 public class BitmapOperationsTests extends AndroidTestCase {
-	public static final String ASSET_FILE_PATH = "file:///android_asset/data.jpg";
-	public static final String ASSET_LARGE_FILE_PATH = "file:///android_asset/large.jpg";
-	public static final String HTTPS_FILE_PATH = "https://dl.dropboxusercontent.com/u/18391781/bookpureandroid.png";
+    public static final String ASSET_FILE_PATH = "file:///android_asset/data.jpg";
+    public static final String ASSET_LARGE_FILE_PATH = "file:///android_asset/large.jpg";
+    public static final String HTTPS_FILE_PATH = "https://dl.dropboxusercontent" +
+            ".com/u/18391781/v2andlib-externaldata/test.jpeg";
 
 
-	@SmallTest
-	public void testLoadBitmapOptions() throws IOException {
-		URI uri = URI.create(ASSET_FILE_PATH);
-		BitmapOperations helper = new BitmapOperations(getContext());
-		BitmapFactory.Options options = helper.getBitmapOptions(uri);
-		assertNotNull("No bitmap options", options);
-		assertEquals("Wrong bitmap width", 480, options.outWidth);
-		assertEquals("Wrong bitmap height", 400, options.outHeight);
-		assertEquals("Wrong bitmap mime", "image/jpeg", options.outMimeType);
+    @SmallTest
+    public void testLoadBitmapOptions() throws IOException {
+        URI uri = URI.create(ASSET_FILE_PATH);
+        BitmapOperations helper = new BitmapOperations(getContext());
+        BitmapFactory.Options options = helper.getBitmapOptions(uri);
+        assertNotNull("No bitmap options", options);
+        assertEquals("Wrong bitmap width", 480, options.outWidth);
+        assertEquals("Wrong bitmap height", 400, options.outHeight);
+        assertEquals("Wrong bitmap mime", "image/jpeg", options.outMimeType);
 
-		Bitmap bitmap = helper.loadBitmap(uri);
-		assertNotNull("No bitmap", bitmap);
-		bitmap.recycle();
-	}
+        Bitmap bitmap = helper.loadBitmap(uri);
+        assertNotNull("No bitmap", bitmap);
+        bitmap.recycle();
+    }
 
-	@SmallTest
-	public void testLoadBitmapOptionsFromHTTPS() throws IOException {
-		URI uri = URI.create(HTTPS_FILE_PATH);
-		BitmapOperations helper = new BitmapOperations(getContext());
-		BitmapFactory.Options options = helper.getBitmapOptions(uri);
-		assertNotNull("No bitmap options", options);
-		assertEquals("Wrong bitmap width", 480, options.outWidth);
-		assertEquals("Wrong bitmap height", 800, options.outHeight);
-		assertEquals("Wrong bitmap mime", "image/png", options.outMimeType);
+    @SmallTest
+    public void testLoadBitmapOptionsFromHTTPS() throws IOException {
+        URI uri = URI.create(HTTPS_FILE_PATH);
+        BitmapOperations helper = new BitmapOperations(getContext());
+        BitmapFactory.Options options = helper.getBitmapOptions(uri);
+        assertNotNull("No bitmap options", options);
+        assertEquals("Wrong bitmap width", 1020, options.outWidth);
+        assertEquals("Wrong bitmap height", 1482, options.outHeight);
+        assertEquals("Wrong bitmap mime", "image/jpeg", options.outMimeType);
 
-		Bitmap bitmap = helper.loadBitmap(uri);
-		assertNotNull("No bitmap", bitmap);
-		bitmap.recycle();
-	}
+        Bitmap bitmap = helper.loadBitmap(uri);
+        assertNotNull("No bitmap", bitmap);
+        bitmap.recycle();
+    }
 
-	@SmallTest
-	public void testScaleFactorCalculations() throws IOException {
-		Rect sourceBitmap = new Rect(0,0,4096,2048);
-		Rect screenRect = new Rect(0, 0, 480, 800);
-		BitmapOperations helper = new BitmapOperations(getContext());
-		float scaleFactor = helper.getScaleFactor(BitmapOperations.ScaleType.FIT_CENTER, screenRect, sourceBitmap);
-		assertTrue("Wrong fit center scale factor", Math.abs(scaleFactor-0.11718) < 0.001);
-		scaleFactor = helper.getScaleFactor(BitmapOperations.ScaleType.CENTER_CROP, screenRect, sourceBitmap);
-		assertTrue("Wrong fit center scale factor", Math.abs(scaleFactor-0.39062) < 0.001);
+    @SmallTest
+    public void testScaleFactorCalculations() throws IOException {
+        Rect sourceBitmap = new Rect(0, 0, 4096, 2048);
+        Rect screenRect = new Rect(0, 0, 480, 800);
+        BitmapOperations helper = new BitmapOperations(getContext());
+        float scaleFactor = helper.getScaleFactor(BitmapOperations.ScaleType.FIT_CENTER, screenRect, sourceBitmap);
+        assertTrue("Wrong fit center scale factor", Math.abs(scaleFactor - 0.11718) < 0.001);
+        scaleFactor = helper.getScaleFactor(BitmapOperations.ScaleType.CENTER_CROP, screenRect, sourceBitmap);
+        assertTrue("Wrong fit center scale factor", Math.abs(scaleFactor - 0.39062) < 0.001);
 
-		URI uri = URI.create(ASSET_LARGE_FILE_PATH);
-		BitmapFactory.Options options = helper.getBitmapScaleOptions(uri, screenRect, BitmapOperations.ScaleType.FIT_CENTER);
-		assertNotNull("No bitmap options", options);
-		assertEquals("Wrong inSample size", 4, options.inSampleSize);
+        URI uri = URI.create(ASSET_LARGE_FILE_PATH);
+        BitmapFactory.Options options = helper.getBitmapScaleOptions(uri, screenRect,
+                BitmapOperations.ScaleType.FIT_CENTER);
+        assertNotNull("No bitmap options", options);
+        assertEquals("Wrong inSample size", 4, options.inSampleSize);
 
-		Bitmap bitmap = helper.loadBitmap(uri, screenRect, BitmapOperations.ScaleType.CENTER_CROP, false);
-		assertNotNull("No bitmap", bitmap);
-		assertTrue("Wrong bitmap size", bitmap.getWidth() <= screenRect.width()
-				|| bitmap.getHeight() <= screenRect.height() );
-		bitmap.recycle();
-		bitmap = helper.loadBitmap(uri, screenRect, BitmapOperations.ScaleType.FIT_CENTER, false);
-		assertNotNull("No bitmap", bitmap);
-		assertTrue("Wrong bitmap size", bitmap.getWidth() <= screenRect.width()
-				&& bitmap.getHeight() <= screenRect.height());
-	}
+        Bitmap bitmap = helper.loadBitmap(uri, screenRect, BitmapOperations.ScaleType.CENTER_CROP, false);
+        assertNotNull("No bitmap", bitmap);
+        assertTrue("Wrong bitmap size", bitmap.getWidth() <= screenRect.width()
+                || bitmap.getHeight() <= screenRect.height());
+        bitmap.recycle();
+        bitmap = helper.loadBitmap(uri, screenRect, BitmapOperations.ScaleType.FIT_CENTER, false);
+        assertNotNull("No bitmap", bitmap);
+        assertTrue("Wrong bitmap size", bitmap.getWidth() <= screenRect.width()
+                && bitmap.getHeight() <= screenRect.height());
+    }
 
-	@SmallTest
-	public void testScale() throws IOException {
-		URI uri = URI.create(ASSET_FILE_PATH);
-		int width = 384;
-		Rect margins = new Rect(0, 0, width, width);
-		BitmapOperations helper = new BitmapOperations(getContext());
-		Bitmap loadedImage = helper.loadBitmap(uri);
-		Bitmap scaledImage = helper.scaleBitmap(loadedImage, margins, BitmapOperations.ScaleType.FIT_CENTER, true);
-		assertNotNull("No bitmap", scaledImage);
-		assertEquals("Wrong bitmap width", 384, scaledImage.getWidth());
-		assertEquals("Wrong bitmap height", 320, scaledImage.getHeight());
-	}
+    @SmallTest
+    public void testScale() throws IOException {
+        URI uri = URI.create(ASSET_FILE_PATH);
+        int width = 384;
+        Rect margins = new Rect(0, 0, width, width);
+        BitmapOperations helper = new BitmapOperations(getContext());
+        Bitmap loadedImage = helper.loadBitmap(uri);
+        Bitmap scaledImage = helper.scaleBitmap(loadedImage, margins, BitmapOperations.ScaleType.FIT_CENTER, true);
+        assertNotNull("No bitmap", scaledImage);
+        assertEquals("Wrong bitmap width", 384, scaledImage.getWidth());
+        assertEquals("Wrong bitmap height", 320, scaledImage.getHeight());
+    }
 }
