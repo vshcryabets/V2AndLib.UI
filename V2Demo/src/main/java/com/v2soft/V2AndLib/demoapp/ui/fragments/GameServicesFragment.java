ffeea47707bd034f340597ed31fa29643ec58235
V.Shcryabets <vshcryabets@gmail.com>
2015-11-28 20:14:00 +0200

    Fix error with wrong sign

--- V2Demo/src/main/java/com/v2soft/V2AndLib/demoapp/ui/fragments/GameServicesFragment.java
+++ V2Demo/src/main/java/com/v2soft/V2AndLib/demoapp/ui/fragments/GameServicesFragment.java
@@ -15,8 +15,11 @@
  */
 package com.v2soft.V2AndLib.demoapp.ui.fragments;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +28,9 @@ import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.games.Games;
+import com.google.android.gms.plus.Plus;
+import com.google.example.games.basegameutils.BaseGameUtils;
+import com.google.example.games.basegameutils.GameHelper;
 import com.v2soft.AndLib.ui.fragments.BaseFragment;
 import com.v2soft.V2AndLib.demoapp.DemoAppSettings;
 import com.v2soft.V2AndLib.demoapp.DemoApplication;
@@ -41,7 +47,19 @@ public class GameServicesFragment
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
 
+    private static final String TAG = GameServicesFragment.class.getSimpleName();
+    private static final int RC_SIGN_IN = 1;
     private GoogleApiClient mGoogleApiClient;
+
+    // Are we currently resolving a connection failure?
+    private boolean mResolvingConnectionFailure = false;
+
+    // Has the user clicked the sign-in button?
+    private boolean mSignInClicked = false;
+
+    // Set to true to automatically start the sign in flow when the Activity starts.
+    // Set to false to require the user to click the button in order to sign in.
+    private boolean mAutoStartSignInFlow = true;
 
     public static Fragment newInstance() {
         return new GameServicesFragment();
@@ -55,9 +73,16 @@ public class GameServicesFragment
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .setViewForPopups(view.findViewById(R.id.popups))
+                .addApi(Plus.API).addScope(Plus.SCOPE_PLUS_LOGIN)
                 .addApi(Games.API).addScope(Games.SCOPE_GAMES)
-                        // add other APIs and scopes here as needed
                 .build();
+        view.findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSignInClicked = true;
+                mGoogleApiClient.connect();
+            }
+        });
         return view;
     }
 
@@ -100,5 +125,34 @@ public class GameServicesFragment
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
         Toast.makeText(getActivity(), "onConnectionFailed " + connectionResult.toString(), Toast.LENGTH_LONG).show();
+        if (mResolvingConnectionFailure) {
+            Log.d(TAG, "onConnectionFailed() ignoring connection failure; already resolving.");
+            return;
+        }
+        if (mSignInClicked || mAutoStartSignInFlow) {
+            mAutoStartSignInFlow = false;
+            mSignInClicked = false;
+            mResolvingConnectionFailure = BaseGameUtils
+                    .resolveConnectionFailure(getActivity(), mGoogleApiClient,
+                            connectionResult, RC_SIGN_IN, getString(R.string.signin_other_error));
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case RC_SIGN_IN:
+                Log.d(TAG, "onActivityResult with requestCode == RC_SIGN_IN, responseCode="
+                        + resultCode + ", intent=" + data);
+                mSignInClicked = false;
+                mResolvingConnectionFailure = false;
+                if (resultCode == Activity.RESULT_OK) {
+                    mGoogleApiClient.connect();
+                } else {
+                    BaseGameUtils.showActivityResultError(getActivity(), requestCode, resultCode, R.string.signin_other_error);
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
